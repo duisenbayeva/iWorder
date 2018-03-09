71b44820ee5f274f9bf4fe072f984b67155cbc41
duisenbayeva <b.duisenbayeva@gmail.com>
2018-03-09 13:28:32 +0600

    started using ion-fab; add button ready

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import {NavController} from 'ionic-angular';
 import {Catalog, CatalogsService} from "../../services/catalogs.service";
 import {NewCatalogPage} from "../new-catalog/new-catalog";
 import {CatalogPage} from "../catalog/catalog";
+import {FabContainer} from "ionic-angular";
 
 
 @Component({
@@ -21,7 +22,8 @@ export class HomePage {
     );
   }
 
-  onClickNewCatalog() {
+  onClickNewCatalog(fab: FabContainer) {
+    fab.close();
     this.navCtrl.push(NewCatalogPage);
   }
 
