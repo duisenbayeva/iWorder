798b9991407ca0c604d15fc56d187490125e7566
duisenbayeva <b.duisenbayeva@gmail.com>
2018-03-17 14:10:41 +0600

    deleteCatalog, deleteWord functions were rewritten by using Map; some optimizations

--- src/services/catalogs.service.ts
+++ src/services/catalogs.service.ts
@@ -19,19 +19,10 @@ export class CatalogsService {
     if (catalogName in this.catalogs2) {
       console.log("catalog exists in map")
     } else {
-      catalogs2[catalogName] = {"catalogName": catalogName, "wordList": {"word": {}}};
+      catalogs2[catalogName] = {"catalogName": catalogName, "wordList": {}};
       console.log("Saved catalog in map", JSON.stringify(catalogs2));
       localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
-      this.catalogs = [];
-      this.catalogs = Object.keys(catalogs2).map(function (val) {
-        console.log("asd ", catalogs2[val]);
-        catalogs2[val].wordList = Object.keys(catalogs2[val].wordList).map(function (val2) {
-          return catalogs2[val].wordList[val2];
-        });
-        return catalogs2[val];
-      });
-      console.log(this.catalogs);
-      this.storage.set('catalogs', this.catalogs);
+      this.updateCatalogList();
     }
   }
 
@@ -56,15 +47,7 @@ export class CatalogsService {
       delete catalogs2[catalogName];
       console.log("Saved catalog in map", JSON.stringify(catalogs2));
       localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
-      this.catalogs = [];
-      this.catalogs = Object.keys(catalogs2).map(function (val) {
-        catalogs2[val].wordList = Object.keys(catalogs2[val].wordList).map(function (val2) {
-          return catalogs2[val].wordList[val2];
-        });
-        return catalogs2[val];
-      });
-      console.log(this.catalogs);
-      this.storage.set('catalogs', this.catalogs);
+      this.updateCatalogList();
     } else {
       console.log("was not found to edit")
     }
@@ -73,34 +56,26 @@ export class CatalogsService {
   addWordToCatalog(word: Word) {
     console.log("addWordToCatalog function input=", word);
     let catalogs2 = localStorage.getItem('catalogsMap') ? JSON.parse(localStorage.getItem('catalogsMap')) : {};
-    let success = false;
+
     if (word.catalog in catalogs2) {
       if (!(word.word in catalogs2[word.catalog].wordList)) {
         catalogs2[word.catalog].wordList[word.word] = word;
         console.log("Word in map", catalogs2[word.catalog].wordList[word.word]);
         console.log("Saved in map", JSON.stringify(catalogs2));
         localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
-        this.catalogs = [];
-        this.catalogs = Object.keys(catalogs2).map(function (val) {
-          catalogs2[val].wordList = Object.keys(catalogs2[val].wordList).map(function (val2) {
-            return catalogs2[val].wordList[val2];
-          });
-          return catalogs2[val];
-        });
-        console.log("List=", this.catalogs);
-        this.storage.set('catalogs', this.catalogs);
+        this.updateCatalogList();
         return;
       }
     }
-    if (!success) {
-      console.log("Word was not added");
-    }
+
+    console.log("Word was not added");
+
   }
 
   editWord(word: Word, newWord: Word) {
     console.log("editWord function input=", word, newWord);
     let catalogs2 = localStorage.getItem('catalogsMap') ? JSON.parse(localStorage.getItem('catalogsMap')) : {};
-    let success = false;
+
     if (word.catalog in catalogs2) {
       if (word.word in catalogs2[word.catalog].wordList) {
         delete catalogs2[word.catalog].wordList[word.word];
@@ -108,72 +83,56 @@ export class CatalogsService {
         console.log("new Word in map", catalogs2[newWord.catalog].wordList[newWord.word]);
         console.log("Saved in map", JSON.stringify(catalogs2));
         localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
-        this.catalogs = [];
-        this.catalogs = Object.keys(catalogs2).map(function (val) {
-          catalogs2[val].wordList = Object.keys(catalogs2[val].wordList).map(function (val2) {
-            return catalogs2[val].wordList[val2];
-          });
-          return catalogs2[val];
-        });
-        console.log("List=", this.catalogs);
-        this.storage.set('catalogs', this.catalogs);
+        this.updateCatalogList();
         return;
       }
     }
-    if (!success) {
-      console.log("Word was not added");
-    }
+    console.log("Word was not added");
+
   }
 
   deleteCatalog(catalogName) {
     console.log("deleteCatalog service input =", catalogName);
-    let catalogDeleted = false;
-    let index = 0;
-    for (let c of this.catalogs) {
-      console.log("c name=", c.catalogName['catalogName'], catalogName)
-      if (c.catalogName['catalogName'] == catalogName.catalogName) {
-        this.catalogs.splice(index, 1);
-        console.log("Found!!!")
-        catalogDeleted = true;
-        this.storage.set('catalogs', this.catalogs);
-        return;
-      }
-      index++;
-    }
-    if (!catalogDeleted) {
-      console.log("Catalog was not deleted error");
+    let catalogs2 = localStorage.getItem('catalogsMap') ? JSON.parse(localStorage.getItem('catalogsMap')) : {};
+    if (catalogName in catalogs2) {
+      delete catalogs2[catalogName];
+      console.log("Saved in map", JSON.stringify(catalogs2));
+      localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
+      this.updateCatalogList();
       return;
     }
-
+    console.log("Catalog was not deleted");
   }
 
   deleteWord(word: Word) {
     console.log("deleteWord service input =", word);
-    let wordDeleted = false;
-    if (word.catalog) {
-      for (let c of this.catalogs) {
-        console.log("c name=", c.catalogName)
-        if (c.catalogName['catalogName'] == word.catalog) {
-          let index = 0;
-          for (let w of c.wordList) {
-            console.log("w=", w.word);
-            if (w.word == word.word) {
-              wordDeleted = true;
-              c.wordList.splice(index, 1);
-              console.log("word exists, deleted", this.catalogs);
-              this.storage.set('catalogs', this.catalogs);
-              return;
-            }
-            index++;
-          }
-          if (!wordDeleted) {
-            console.log("WORD was not deleted error");
-            return;
-          }
-        }
+    let catalogs2 = localStorage.getItem('catalogsMap') ? JSON.parse(localStorage.getItem('catalogsMap')) : {};
+
+    if (word.catalog in catalogs2) {
+      if (word.word in catalogs2[word.catalog].wordList) {
+        delete catalogs2[word.catalog].wordList[word.word];
+        console.log("Saved in map", JSON.stringify(catalogs2));
+        localStorage.setItem('catalogsMap', JSON.stringify(catalogs2));
+        this.updateCatalogList();
+        return;
       }
     }
+    console.log("Word was not deleted");
   }
+
+  updateCatalogList = function () {
+    let catalogs2 = localStorage.getItem('catalogsMap') ? JSON.parse(localStorage.getItem('catalogsMap')) : {};
+    this.catalogs = [];
+    this.catalogs = Object.keys(catalogs2).map(function (val) {
+      catalogs2[val].wordList = Object.keys(catalogs2[val].wordList).map(function (val2) {
+        return catalogs2[val].wordList[val2];
+      });
+      return catalogs2[val];
+    });
+    console.log("List=", this.catalogs);
+    this.storage.set('catalogs', this.catalogs);
+  }
+
 }
 
 
