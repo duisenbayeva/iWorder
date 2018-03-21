5901ca8f7d53ead83b6af56dde526a4c7331fae9
duisenbayeva <b.duisenbayeva@gmail.com>
2018-03-21 20:16:39 +0600

    catalog page- new; edit & delete fabs, no open button

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -2,7 +2,6 @@ import {Component} from "@angular/core";
 import {FabContainer, IonicPage, NavController, NavParams} from "ionic-angular";
 import {CatalogsService} from "../../services/catalogs.service";
 import {NewWordPage} from "../new-word/new-word";
-import {NewCatalogPage} from "../new-catalog/new-catalog";
 import {Word} from "../../model/word.model";
 
 @IonicPage()
@@ -13,6 +12,8 @@ import {Word} from "../../model/word.model";
 export class CatalogPage {
   private name: string = "";
   private words: Word[] = [];
+  private editMode: boolean = false;
+  private deleteMode: boolean = false;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -28,6 +29,35 @@ export class CatalogPage {
   addWord(fab: FabContainer) {
     fab.close();
     this.navCtrl.push(NewWordPage, {create: true, catalogName: this.name, wordList: this.words});
+  }
+
+  editWordMode(fab: FabContainer) {
+    fab.close();
+    this.deleteMode = false;
+    this.editMode = !this.editMode;
+  }
+
+  deleteWordMode(fab: FabContainer) {
+    fab.close();
+    this.editMode = false;
+    this.deleteMode = !this.deleteMode;
+  }
+
+  editWord(word, fab: FabContainer) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("edit word func", word);
+    fab.close();
+    this.navCtrl.push(NewWordPage, {create: false, catalogName: this.name, word: word});
+  }
+
+  deleteWord(word, fab: FabContainer){
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("delete word func", word);
+    fab.close();
+    this.catalogService.deleteWord(word);
+    this.navCtrl.pop();
   }
 
   openWord(word: Word, fab: FabContainer) {
